52a515314571196f6cc2f52dcec76d306248a628
sdkei <sdkei0xb@gmail.com>
2025-05-17 13:25:30 +0900

    chore: dependencies に CameraX ライブラリーを追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,21 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    // CameraX core library using the camera2 implementation
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation(libs.androidx.camera.lifecycle)
+    // If you want to additionally use the CameraX VideoCapture library
+    implementation(libs.androidx.camera.video)
+    // If you want to additionally use the CameraX View class
+    implementation(libs.androidx.camera.view)
+    // If you want to additionally add CameraX ML Kit Vision Integration
+    implementation(libs.androidx.camera.mlkit.vision)
+    // If you want to additionally use the CameraX Extensions library
+    implementation(libs.androidx.camera.extensions)
+
     testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.junit)
