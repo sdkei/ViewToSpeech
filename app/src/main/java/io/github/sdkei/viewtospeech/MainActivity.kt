b6a6e666f906b77ba13d6ecddc401454cf955c31
sdkei <sdkei0xb@gmail.com>
2025-05-18 15:23:30 +0900

    feat: カメラプレビュー composable を実装

--- app/src/main/java/io/github/sdkei/viewtospeech/MainActivity.kt
+++ app/src/main/java/io/github/sdkei/viewtospeech/MainActivity.kt
@@ -1,9 +1,14 @@
 package io.github.sdkei.viewtospeech
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,7 +16,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import com.google.common.util.concurrent.ListenableFuture
 import io.github.sdkei.viewtospeech.ui.theme.ViewToSpeechTheme
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.guava.await
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,4 +56,50 @@ fun GreetingPreview() {
     ViewToSpeechTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun CameraPreview(
+    cameraProviderFuture: ListenableFuture<ProcessCameraProvider>,
+    imageCapture: ImageCapture,
+    modifier: Modifier = Modifier,
+    onError: (Throwable) -> Unit = {},
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    AndroidView(
+        factory = { context ->
+            val previewView = PreviewView(context)
+
+            val cameraSelector = CameraSelector.Builder()
+                // 背面カメラ
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+            val preview = androidx.camera.core.Preview.Builder().build().apply {
+                surfaceProvider = previewView.surfaceProvider
+            }
+
+            lifecycleOwner.lifecycleScope.launch(
+                CoroutineExceptionHandler { _, e ->
+                    Log.e("CameraPreview", "カメラプレビューを初期化できませんでした。", e)
+                    onError(e)
+                }
+            ) {
+                cameraProviderFuture.await().apply {
+                    // 以前のバインディングを解除
+                    unbindAll()
+
+                    bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview,
+                        imageCapture,
+                    )
+                }
+            }
+
+            previewView
+        },
+        modifier = modifier,
+    )
 }
